13cf5d18ed7f94904eb75e30ea1789fedc0990ff
Wellington Grisa <wellington.grisa@gmail.com>
2023-12-22 12:09:10 -0300

    include ui and api images as secrets in ci

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,9 @@ set -e
 deploy_to_elastic() {
   pip install awsebcli
 
+  sed -e "s/\${API_IMAGE}/$API_IMAGE/g" docker-compose.template.yml > docker-compose.yml
+  sed -e "s/\${UI_IMAGE}/$UI_IMAGE/g" docker-compose.template.yml > docker-compose.yml
+
   zip deploy.zip docker-compose.yml default.conf -r
 
   eb deploy
