80dcc6ab986367e73a73190eb8e54c7a5dd8246f
Wellington Grisa <wellington.grisa@gmail.com>
2023-12-22 12:24:03 -0300

    change

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,7 +5,7 @@ set -e
 deploy_to_elastic() {
   pip install awsebcli
 
-  sed -e "s/\${API_IMAGE}/$API_IMAGE/g" -e "s/\${UI_IMAGE}/$UI_IMAGE/g" docker-compose.template.yml > docker-compose.yml
+  sed -e "s/\${API_IMAGE}/BANANA/g" -e "s/\${UI_IMAGE}/APPLE/g" docker-compose.template.yml > docker-compose.yml
 
   cat docker-compose.yml
 
