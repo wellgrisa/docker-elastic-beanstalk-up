83be5ae47bff7da597e8f3f8a9479795e95a07f6
Wellington Grisa <wellington.grisa@gmail.com>
2023-12-28 12:47:24 -0300

    change

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,23 +5,13 @@ set -e
 deploy_to_elastic() {
   pip install awsebcli
 
-  API_IMAGE=$REGISTRY/$REPOSITORY:api-$GITHUB_REF_NAME
-  UI_IMAGE=$REGISTRY/$REPOSITORY:ui-$GITHUB_REF_NAME
+  TAG=$PACKAGE_NAME-$GITHUB_REF_NAME
 
-  echo ">>>>>"
-  echo $REGISTRY
-  echo $REPOSITOY
+  $IMAGE=$REGISTRY/$REPOSITORY:$TAG
 
-  ESCAPED_API_IMAGE=$(echo "$API_IMAGE" | sed 's/[\/&]/\\&/g')
-  ESCAPED_UI_IMAGE=$(echo "$UI_IMAGE" | sed 's/[\/&]/\\&/g')
+  sed -e "s/\${IMAGE}/$IMAGE/g" Dockerrun.template.aws.json > Dockerrun.aws.json
 
-  sed -e "s/\${API_IMAGE}/$ESCAPED_API_IMAGE/g" -e "s/\${UI_IMAGE}/$ESCAPED_UI_IMAGE/g" docker-compose.template.yml > docker-compose.yml
-
-  cat docker-compose.yml
-
-  zip deploy.zip docker-compose.yml default.conf -r
-
-  eb deploy
+  eb deploy --staged
 }
 
 deploy_to_elastic
