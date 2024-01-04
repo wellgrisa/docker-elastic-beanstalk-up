9ecdb0ce9d87ca61816a0eac7d9f65e06f1aac6a
Wellington Grisa <wellington.grisa@gmail.com>
2024-01-04 12:00:10 -0300

    chore: still testing

--- ci/test.sh
+++ ci/test.sh
@@ -2,13 +2,28 @@
 
 set -e
 
-echo ">>>"
+deploy_to_elastic() {
+  # pip install awsebcli
 
-variable_name=$(git describe --tags --match="api*" --abbrev=0)
+  LATEST_API_TAG=$(git describe --tags --match="api*" --abbrev=0)
+  LATEST_UI_TAG=$(git describe --tags --match="ui*" --abbrev=0)
 
-# Now 'variable_name' contains the result of the 'git describe' command
-echo "The result is: $variable_name"
+  API_IMAGE_TAG="${LATEST_API_TAG//@/-}"
+  UI_IMAGE_TAG="${LATEST_UI_TAG//@/-}"
 
-git describe --tags --match="api*"  --abbrev=0
+  API_IMAGE=$REGISTRY/$REPOSITORY:$API_IMAGE_TAG
+  UI_IMAGE=$REGISTRY/$REPOSITORY:$UI_IMAGE_TAG
 
-echo "<<<<"
+  ESCAPED_API_IMAGE=$(echo "$API_IMAGE" | sed 's/[\/&]/\\&/g')
+  ESCAPED_UI_IMAGE=$(echo "$UI_IMAGE" | sed 's/[\/&]/\\&/g')
+
+  sed -e "s/\${API_IMAGE}/$ESCAPED_API_IMAGE/g" -e "s/\${UI_IMAGE}/$ESCAPED_UI_IMAGE/g" docker-compose.template.yml > docker-compose.yml
+
+  cat docker-compose.yml
+
+  # zip deploy.zip docker-compose.yml default.conf -r
+
+  # eb deploy
+}
+
+deploy_to_elastic
