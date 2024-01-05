4edb398c727fcd2851dc558d00b0c570b6bca72a
Wellington Grisa <wellington.grisa@gmail.com>
2024-01-05 10:46:10 -0300

    chore(api): change something

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use("/", (_req, res) =>
   res.status(200).send({
-    pong: "this pong is coming from api (from version) - finally",
+    pong: "this pong is coming from api (from version) :D",
   } as Pong)
 );
 
