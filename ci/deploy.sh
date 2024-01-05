0eda00d98b925bc2b18a5dfc17c2b70c455c107b
Wellington Grisa <wellington.grisa@gmail.com>
2024-01-04 22:48:57 -0300

    chore(ui): change something

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,11 +3,8 @@
 set -e
 
 deploy_to_elastic() {
-  # pip install awsebcli
-  echo ">>>>>>>>"
-
-  sleep 2
-
+  pip install awsebcli
+  
   LATEST_API_TAG=$(git describe --tags --match="api*" --abbrev=0)
   LATEST_UI_TAG=$(git describe --tags --match="ui*" --abbrev=0)
 
@@ -34,35 +31,32 @@ deploy_to_elastic() {
     REPOSITORY_IMAGE_TAG_TO_CHECK=$API_IMAGE_TAG 
   fi
 
-  echo $REPOSITORY_IMAGE_TAG_TO_CHECK
-
-  echo "<<><><><"
-
   while ! aws ecr describe-images \
     --repository-name $REPOSITORY \
     --image-ids imageTag="$REPOSITORY_IMAGE_TAG_TO_CHECK" >/dev/null 2>&1; do
     echo "ECR image does not exist. Retrying..."
-    sleep 1 # Adjust the sleep duration as needed
+    sleep 10
   done
 
-  echo "ECR image exists."
+  zip deploy.zip docker-compose.yml default.conf -r
 
-  # aws ecr describe-images --repository-name "$(echo "184503795422.dkr.ecr.us-east-1.amazonaws.com/docker-elastic-beanstalk-up" | cut -d/ -f2)" --image-ids imageTag="$API_IMAGE"
+  while true; do
+    STATUS=$(eb status docker-elastic-beanstalk-up-dev | grep -i status | awk '{print $2}')
 
-  # zip deploy.zip docker-compose.yml default.conf -r
+    if [ "$STATUS" == "Ready" ]; then
+        echo "Elastic Beanstalk is ready!"
+    elif [ "$STATUS" == "Pending" ]; then
+        echo "Elastic Beanstalk is still pending... Exiting."
+        exit 0
+    else
+        echo "Unknown status. Exiting with an error."
+        exit 1
+    fi
 
-  # while true; do
-  #   eb status docker-elastic-beanstalk-up-dev | grep -i status | grep -q "Ready"
-  #   if [ $? -eq 0 ]; then
-  #     echo "Elastic Beanstalk is ready!"
-  #     break
-  #   else
-  #     echo "Waiting for Elastic Beanstalk to be ready..."
-  #     sleep 20 
-  #   fi
-  # done
+    sleep 10
+  done
 
-  # eb deploy docker-elastic-beanstalk-up-dev
+  eb deploy docker-elastic-beanstalk-up-dev
 }
 
 deploy_to_elastic
